dd7b0fa762a9478d6f94145a70c04a97b7b6aa00
SHUBHAM5691 <104740076+SHUBHAM5691@users.noreply.github.com>
2025-06-23 15:44:32 +0530

    Senior Citizen Details page

---
+++ second_innings/lib/dashboard/family/views/senior_citizens_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SeniorCitizensDetail extends StatefulWidget {
+  const SeniorCitizensDetail({super.key});
+
+  @override
+  State<SeniorCitizensDetail> createState() => _SeniorCitizensDetailState();
+}
+
+class _SeniorCitizensDetailState extends State<SeniorCitizensDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
