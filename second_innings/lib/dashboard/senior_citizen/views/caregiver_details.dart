f3ecdcad716076a6fece1c02b39535589e3b5361
SHUBHAM5691 <104740076+SHUBHAM5691@users.noreply.github.com>
2025-06-23 16:45:41 +0530

    Caregiver details(Senior Citizen)

---
+++ second_innings/lib/dashboard/senior_citizen/views/caregiver_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CaregiverDetails extends StatefulWidget {
+  const CaregiverDetails({super.key});
+
+  @override
+  State<CaregiverDetails> createState() => _CaregiverDetailsState();
+}
+
+class _CaregiverDetailsState extends State<CaregiverDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
