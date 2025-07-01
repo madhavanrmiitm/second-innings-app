f7ba39f8abb0f48e44ae533c48b4728b76ddb8af
Ashrockzzz2003 <ashrockzzz2003@gmail.com>
2025-07-01 11:19:49 +0530

    format: code with dart format.

--- second_innings/lib/dashboard/senior_citizen/views/family_view.dart
+++ second_innings/lib/dashboard/senior_citizen/views/family_view.dart
@@ -3,7 +3,7 @@ import 'package:second_innings/dashboard/senior_citizen/views/link_new_family_me
 
 class FamilyView extends StatelessWidget {
   const FamilyView({super.key});
-  
+
   // Sample data for family members
   final List<Map<String, String>> familyMembers = const [
     {'name': 'John Doe', 'relationship': 'Son'},
@@ -15,15 +15,15 @@ class FamilyView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Family Members'),
-      ),
+      appBar: AppBar(title: const Text('Family Members')),
       body: ListView.builder(
         itemCount: familyMembers.length,
         itemBuilder: (context, index) {
           final member = familyMembers[index];
           return ListTile(
-            leading: const Icon(Icons.person), // You can replace this with a profile picture
+            leading: const Icon(
+              Icons.person,
+            ), // You can replace this with a profile picture
             title: Text(member['name']!),
             subtitle: Text(member['relationship']!),
             // Add onTap functionality if needed
@@ -35,7 +35,9 @@ class FamilyView extends StatelessWidget {
           // Navigate to the LinkNewFamilyMemberPage
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => const LinkNewFamilyMemberPage()),
+            MaterialPageRoute(
+              builder: (context) => const LinkNewFamilyMemberPage(),
+            ),
           );
         },
         label: const Text('Link New'),
